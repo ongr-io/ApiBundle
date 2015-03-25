928ed95cdb95f07e7246904e4be5cb8f139a1c11
Erik Maslak <emaslak@nfq.lt>
2015-03-25 18:29:01 +0200

    Api extension and configuration update.
    - Added ability to set endpoint parents in config.

--- DependencyInjection/ONGRApiExtension.php
+++ DependencyInjection/ONGRApiExtension.php
@@ -12,6 +12,7 @@
 namespace ONGR\ApiBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
@@ -32,6 +33,13 @@ class ONGRApiExtension extends Extension
         foreach ($config['versions'] as $versionName => $version) {
             $container->setParameter("ongr_api.$versionName.endpoints", array_keys($version['endpoints']));
             foreach ($version['endpoints'] as $endpointName => $endpoint) {
+                if (isset($endpoint['parent'])) {
+                    $endpoint = $this->appendParentConfig($endpoint, $endpoint['parent'], $version['endpoints']);
+                } elseif ($endpoint['manager'] === null) {
+                    throw new InvalidConfigurationException(
+                        "No manager set for endpoint '$endpointName'."
+                    );
+                }
                 $container->setParameter("ongr_api.$versionName.$endpointName.manager", $endpoint['manager']);
                 $container->setParameter("ongr_api.$versionName.$endpointName.documents", $endpoint['documents']);
                 $container->setParameter("ongr_api.$versionName.$endpointName.controller", $endpoint['controller']);
@@ -40,5 +48,34 @@ class ONGRApiExtension extends Extension
 
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
+    }
+
+    /**
+     * Appends documents to endpoint from parent endpoint.
+     *
+     * @param array  $endpoint
+     * @param string $parentName
+     * @param array  $endpoints
+     *
+     * @return array
+     * @throws InvalidConfigurationException
+     */
+    private function appendParentConfig($endpoint, $parentName, $endpoints)
+    {
+        if (!array_key_exists($parentName, $endpoints)) {
+            throw new InvalidConfigurationException(
+                "Invalid parent endpoint '$parentName'."
+            );
+        }
+        $parent = $endpoints[$parentName];
+        if (isset($parent['parent'])) {
+            $parent = $this->appendParentConfig($parent, $parent['parent'], $endpoints);
+        }
+        $endpoint['documents'] = array_unique(array_merge($endpoint['documents'], $parent['documents']));
+        if ($endpoint['manager'] === null) {
+            $endpoint['manager'] = $parent['manager'];
+        }
+
+        return $endpoint;
     }
 }
