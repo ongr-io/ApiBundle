f60b1c0e346757584b42ca0e3136d533403706b8
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2015-03-25 13:50:03 +0200

    Add tests

---
+++ Tests/app/AppKernel.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+
+/**
+ * App for testing.
+ */
+class AppKernel extends Kernel
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function registerBundles()
+    {
+        return [
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+
+            new ONGR\ElasticsearchBundle\ONGRElasticsearchBundle(),
+            new ONGR\ApiBundle\ONGRApiBundle(),
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/config/config.yml');
+        $loader->load(__DIR__ . '/config/services.yml');
+    }
+}
