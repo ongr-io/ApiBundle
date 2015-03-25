2e1a782923bd66a95460196e4038277e3fd9049c
Gediminas Šaltenis <gediminas.saltenis@ongr.io>
2015-03-25 10:20:52 +0200

    Fix bad spacing

--- Controller/ApiControllerInterface.php
+++ Controller/ApiControllerInterface.php
@@ -1,13 +1,13 @@
 <?php
 
 /*
-* This file is part of the ONGR package.
-*
-* (c) NFQ Technologies UAB <info@nfq.com>
-*
-* For the full copyright and license information, please view the LICENSE
-* file that was distributed with this source code.
-*/
+ * This file is part of the ONGR package.
+ *
+ * (c) NFQ Technologies UAB <info@nfq.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace ONGR\ApiBundle\Controller;
 
