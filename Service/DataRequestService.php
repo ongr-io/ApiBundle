54f55cb85fce95ffb12a5a784057a8a2ecd08bcb
Aivaras Gotovskis <aivaras.gotovskis@ongr.io>
2015-03-30 13:41:47 +0300

    Move response creation to DataRequestService.

--- Service/DataRequestService.php
+++ Service/DataRequestService.php
@@ -11,11 +11,14 @@
 
 namespace ONGR\ApiBundle\Service;
 
+use JMS\Serializer\SerializerBuilder;
 use ONGR\ElasticsearchBundle\ORM\Repository;
 use Symfony\Component\DependencyInjection\Container;
 use ONGR\ElasticsearchBundle\ORM\Manager;
 use ONGR\ElasticsearchBundle\DSL\Query\MatchAllQuery;
 use ONGR\ElasticsearchBundle\DSL\Search;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class DataRequestService, for managing request to a Document repository.
@@ -67,4 +70,58 @@ class DataRequestService
 
         return $this->dataRepository->execute($search);
     }
+
+    /**
+     * Creates response for get requests.
+     *
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function getResponse(Request $request)
+    {
+        $serializer = SerializerBuilder::create()->build();
+        $format = $this->getResponseFormat($request);
+        $mime = $this->getResponseMime($format);
+
+        $data = $this->get($request);
+        $data = $serializer->serialize($data, $format);
+
+        $response = new Response();
+        $response->setContent($data);
+        $response->headers->set('Content-Type', $mime);
+
+        return $response;
+    }
+
+    /**
+     * @param Request $request
+     *
+     * @return string
+     */
+    private function getResponseFormat(Request $request)
+    {
+        return 'json';
+    }
+
+    /**
+     * @param string $format
+     *
+     * @return string
+     *
+     * @throws \DomainException
+     */
+    private function getResponseMime($format)
+    {
+        static $types = [
+            'json' => 'application/json',
+        ];
+
+        if (!isset($types[$format])) {
+            throw new \DomainException("Unknown format \"{$format}\"");
+        }
+
+        return $types[$format];
+    }
+
 }
