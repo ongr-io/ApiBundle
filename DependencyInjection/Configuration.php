76a610cffb0fd89d3dac51cdb23cd3d977165f5a
Erik Maslak <emaslak@nfq.lt>
2015-04-08 18:03:06 +0300

    Added configuration information & examples.

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -30,61 +30,95 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('versions')
+                    ->info('Defines api versions.')
                     ->isRequired()
                     ->requiresAtLeastOneElement()
-                    ->example(
-                        [
-                            'version_1' => [
-                                'endpoints' => [
-                                    'endpoint_1' => [
-                                        'manager' => 'es.manager.default',
-                                        'document' => 'ONGRDemoBundle:ProductDocument',
-                                    ],
-                                ],
-                            ],
-                        ]
-                    )
                     ->useAttributeAsKey('version')
                     ->prototype('array')
                         ->children()
                             ->scalarNode('version')->end()
                             ->arrayNode('endpoints')
+                                ->info('Defines version endpoints.')
                                 ->isRequired()
                                 ->requiresAtLeastOneElement()
                                 ->useAttributeAsKey('endpoint')
 
                                 ->prototype('array')
+                                    ->info('Holds endpoint config.')
                                     ->children()
                                         ->scalarNode('endpoint')->end()
-                                        ->scalarNode('manager')->end()
-                                        ->scalarNode('document')->end()
+                                        ->scalarNode('manager')
+                                            ->info('Elastic search manager name.')
+                                            ->example('es.manager.default')
+                                        ->end()
+                                        ->scalarNode('document')
+                                            ->info('Logical document class name.')
+                                            ->example('ONGRDemoBundle:ProductDocument')
+                                        ->end()
                                         ->arrayNode('include_fields')
                                             ->prototype('scalar')->end()
+                                            ->info(
+                                                'Document fields to include. Can not be used with \'exclude_fields\'.'
+                                            )
+                                            ->example(['title', 'price'])
                                         ->end()
                                         ->arrayNode('exclude_fields')
                                             ->prototype('scalar')->end()
+                                            ->info(
+                                                'Document fields to exclude. Can not be used with \'include_fields\'.'
+                                            )
+                                            ->example(['id', 'slug'])
                                         ->end()
                                         ->arrayNode('controller')
+                                            ->info('Defines controller to override default one.')
                                             ->children()
                                                 ->scalarNode('name')
                                                     ->isRequired()
+                                                    ->info('Logical controller name.')
+                                                    ->example('ONGRDemoBundle:CustomApi')
                                                 ->end()
-                                                ->scalarNode('path')->end()
+                                                ->scalarNode('path')
+                                                    ->info('Routing path, where endpoint will be available.')
+                                                    ->example('/products/{id}/{field}')
+                                                ->end()
                                                 ->arrayNode('defaults')
                                                     ->prototype('variable')->end()
+                                                    ->info('Route parameters defaults.')
+                                                    ->example(
+                                                        [
+                                                            'id' => 1,
+                                                            'field' => 'title',
+                                                        ]
+                                                    )
                                                 ->end()
                                                 ->arrayNode('requirements')
+                                                    ->info('Route requirements.')
                                                     ->prototype('variable')->end()
                                                 ->end()
                                                 ->arrayNode('options')
+                                                    ->info('Route options.')
                                                     ->prototype('variable')->end()
                                                 ->end()
                                                 ->arrayNode('params')
+                                                    ->info('Anything you want to pass to controller as parameters.')
+                                                    ->example(
+                                                        [
+                                                            'simple_param' => 123,
+                                                            'array_param' =>
+                                                                [
+                                                                    'some_string' => 'string',
+                                                                    'some_int' => 321,
+                                                                ],
+                                                        ]
+                                                    )
                                                     ->prototype('variable')->end()
                                                 ->end()
                                             ->end()
                                         ->end()
-                                        ->scalarNode('parent')->end()
+                                        ->scalarNode('parent')
+                                            ->info('Defines endpoint from which configuration will be inherited.')
+                                            ->example('product')
+                                        ->end()
                                     ->end()
                                 ->end()
                             ->end()
