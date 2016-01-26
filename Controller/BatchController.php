6aa17dc39fbe9afaa3b453d9e7d8d64fc92ae8a4
Simonas Šerlinskas <simonas.serlinskas@gmail.com>
2016-01-26 15:09:53 +0200

    cleaned batch controller

--- Controller/BatchController.php
+++ Controller/BatchController.php
@@ -16,85 +16,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
- * Abstraction for rest api controller.
+ * Batch controller
  */
-class AbstractRestController extends Controller
+class BatchController extends Controller
 {
-
-    /** @var  Crud $crud */
-    private $crud;
-
-    /**
-     * Get CRUD Service
-     *
-     * @return Crud
-     */
-    public function getCrud()
-    {
-
-        if (!$this->crud) {
-            if (!$this->container->has('ongr_api.crud')) {
-                throw new \RuntimeException('Please set RESTful CRUD Service.');
-            }
-            $this->crud = $this->container->get('ongr_api.crud');
-        }
-
-        return $this->crud;
-    }
-
-    /**
-     * Set CRUD Service
-     *
-     * @param Crud $crud
-     */
-    public function setCrud(Crud $crud)
-    {
-        $this->crud = $crud;
-    }
-
-    /**
-     * Renders rest response.
-     *
-     * @param mixed $data
-     * @param int $statusCode
-     * @param array $headers
-     *
-     * @return Response|array
-     */
-    public function renderRest(
-        $data,
-        $statusCode = Response::HTTP_OK,
-        $headers = []
-    ) {
-
-        if ($data == null) {
-            return $this->renderError("NOT FOUND", Response::HTTP_NOT_FOUND);
-        }
-
-        return $this->get('ongr_api.rest_response_view_handler')
-            ->handleView($data, $statusCode, $headers);
-    }
-
-    /**
-     * Error Response
-     *
-     * @param string $message
-     * @param int $statusCode
-     * @return array|Response
-     */
-    public function renderError(
-        $message,
-        $statusCode = Response::HTTP_BAD_REQUEST
-    ) {
-
-        // TODO: Add more information about this Error
-
-        $response = [
-            'errors' => [],
-            'message' => $message,
-            'code' => $statusCode
-        ];
-
-        return $this->renderRest($response, $statusCode);
-    }
 }
